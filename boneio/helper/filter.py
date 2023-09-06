424501dd261f96f1d8e0d6c803e07500fc52891b
wedr0wycz <98967693+wedr0wycz@users.noreply.github.com>
2023-09-06 15:54:40 +0200

    revert

--- boneio/helper/filter.py
+++ boneio/helper/filter.py
@@ -7,8 +7,8 @@ _LOGGER = logging.getLogger(__name__)
 
 FILTERS = {
     "offset": lambda x, y: x + y,
-    "multiply": lambda x, y: x * y,
     "round": lambda x, y: round(x, y),
+    "multiply": lambda x, y: x * y,
     "filter_out": lambda x, y: None if x == y else x,
     "filter_out_greater": lambda x, y: None if x > y else x,
     "filter_out_lower": lambda x, y: None if x < y else x,
