4d2676af526ec2c972a10d14f4a6eb30a93f2330
wedr0wycz <98967693+wedr0wycz@users.noreply.github.com>
2023-09-06 13:20:38 +0200

    round at the end

--- boneio/helper/filter.py
+++ boneio/helper/filter.py
@@ -7,11 +7,11 @@ _LOGGER = logging.getLogger(__name__)
 
 FILTERS = {
     "offset": lambda x, y: x + y,
-    "round": lambda x, y: round(x, y),
     "multiply": lambda x, y: x * y,
     "filter_out": lambda x, y: None if x == y else x,
     "filter_out_greater": lambda x, y: None if x > y else x,
     "filter_out_lower": lambda x, y: None if x < y else x,
+    "round": lambda x, y: round(x, y),
 }
 
 
