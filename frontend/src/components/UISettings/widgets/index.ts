842e1fa63296be053aca1a591067a7116c801892
pszafer <pszafer@gmail.com>
2026-03-25 14:54:02 +0100

    form input indexes

---
+++ frontend/src/components/UISettings/widgets/index.ts
@@ -0,0 +1,8 @@
+// Reusable form input widgets for UISettings
+export { FormInputNumber } from './FormInputNumber';
+export { FormInputText } from './FormInputText';
+export { FormInputSelect } from './FormInputSelect';
+export { FormInputToggle } from './FormInputToggle';
+
+// Existing widgets
+export { default as SimpleTimePeriodInput } from './SimpleTimePeriodInput';
