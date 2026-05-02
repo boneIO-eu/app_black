717edc5fd6513b3fe9d31632f2d6c7f5b4759cd5
patryk84a <patryk-84a@o2.pl>
2026-05-02 15:32:00 +0200

    fix max long press duration

--- frontend/src/components/UISettings/EventForm.tsx
+++ frontend/src/components/UISettings/EventForm.tsx
@@ -790,10 +790,10 @@ const EventForm: React.FC<EventFormProps> = ({
             <div className="form-control">
               <SimpleTimePeriodInput
                 label={t('event_form.max_long_press_duration')}
-                value={data.max_long_press_duration || '120s'}
+                value={data.max_long_press_duration ?? '120s'}
                 onChange={(value) => updateField('max_long_press_duration', value)}
-                maximum={600}
-                allowedUnits={['s']}
+                maximum={600000}
+                allowedUnits={['s', 'ms']}
               />
               <label className="label">
                 <span className="label-text-alt">{t('event_form.max_long_press_duration_hint')} ({t('common.default')}: 120s)</span>
